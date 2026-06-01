0400f5de7fb0bd3bf56240dd17ceb0b08eba05f9
[gotitaxd] <[mariabelencoa@gmail.com]>
2026-06-01 08:21:06 -0400

    Primer commit

---
+++ Source/NavesUSFX2026/InterfazObserver.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InterfazObserver.h"
+
+// Add default functionality here for any IInterfazObserver functions that are not pure virtual.
